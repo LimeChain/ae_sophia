41bf800f663f7b761d77d136dd2e05d02030d3cd
vsavovlime <veselin@limechain.tech>
2018-11-19 16:54:06 +0200

    Added unit tests for erc20

--- test/erc721.js
+++ test/erc721.js
@@ -7,33 +7,11 @@ const readFileRelative = require('./utils').readFileRelative;
 const writeFileRelative = require('./utils').writeFileRelative;
 const AeSDK = require('@aeternity/aepp-sdk');
 const Ae = AeSDK.Universal;
-
-const config = {
-	host: "http://localhost:3001/",
-	internalHost: "http://localhost:3001/internal/",
-	ownerKeyPair: {
-		secretKey: 'bb9f0b01c8c9553cfbaf7ef81a50f977b1326801ebf7294d1c2cbccdedf27476e9bbf604e611b5460a3b3999e9771b6f60417d73ce7c5519e12f7e127a1225ca',
-		publicKey: 'ak_2mwRmUeYmfuW93ti9HMSUJzCk1EYcQEfikVSzgo6k2VghsWhgU'
-	},
-	notOwnerKeyPair: {
-		secretKey: 'e37484af730bc798ac10fdce7523dc24a64182dfe88ff139f739c1c7f3475434df473b854e8d78394c20abfcb8fda9d0ed5dff8703d8668dccda9be157a60b6d',
-		publicKey: 'ak_2hLLun8mZQvbEhaDxaWtJBsXLnhyokynwfMDZJ67TbqGoSCtQ9'
-	},
-	notOwnerPubKeyHex: "0xdf473b854e8d78394c20abfcb8fda9d0ed5dff8703d8668dccda9be157a60b6d",
-	pubKeyHex: '0xe9bbf604e611b5460a3b3999e9771b6f60417d73ce7c5519e12f7e127a1225ca',
-	filesEncoding: 'utf-8',
-	sourceFile: './contracts/erc721/erc721_full.aes',
-	gas: 200000,
-	ttl: 55
-}
-
+const config = require("./config.json")
+const sourceFile = './contracts/erc721/erc721_full.aes';
 const tokenName = "Lime Token";
 const tokenSymbol = "NFT";
-const gasUsed = 100000;
 const firstTokenId = 0;
-const secondTokenId = 1;
-const thirdTokenId = 2;
-const nonExistentTokenId = 123;
 
 describe('ERC721', () => {
 
