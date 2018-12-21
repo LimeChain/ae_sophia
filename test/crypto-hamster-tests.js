ac2db0bb1fc4fb3c256dcbf1752ef8d24c9e606f
Aleksey <alexeitcekov@abv.bg>
2018-12-21 17:52:17 +0200

    fix comments, rename some variables

--- test/crypto-hamster-tests.js
+++ test/crypto-hamster-tests.js
@@ -35,7 +35,7 @@ describe('Crypto Hamsters', async () => {
     it('Should create hamster successfully', async () => {
         await executeSmartContractFunction(cryptoHamsterInstance, 'createHamster', `("${randomNames[0]}")`);
 
-        let result = await executeSmartContractFunction(cryptoHamsterInstance, 'isNameExist', `("${randomNames[0]}")`);
+        let result = await executeSmartContractFunction(cryptoHamsterInstance, 'nameExists', `("${randomNames[0]}")`);
         let resultValue = (await result.decode('bool')).value;
         assert.ok(resultValue === 1, 'Hamster does not exist!');
     });
@@ -44,7 +44,7 @@ describe('Crypto Hamsters', async () => {
         for (let i = 0; i < randomNames.length; i++) {
             await executeSmartContractFunction(cryptoHamsterInstance, 'createHamster', `("${randomNames[i]}")`);
 
-            let result = await executeSmartContractFunction(cryptoHamsterInstance, 'isNameExist', `("${randomNames[i]}")`);
+            let result = await executeSmartContractFunction(cryptoHamsterInstance, 'nameExists', `("${randomNames[i]}")`);
             let resultValue = (await result.decode('bool')).value;
             assert.ok(resultValue === 1, 'Hamster does not exist!');
         }
@@ -58,7 +58,7 @@ describe('Crypto Hamsters', async () => {
     it('Hamster (name) should NOT exist', async () => {
 
         for (let i = 0; i < randomNames.length; i++) {
-            let result = await executeSmartContractFunction(cryptoHamsterInstance, 'isNameExist', `("${randomNames[i]}")`);
+            let result = await executeSmartContractFunction(cryptoHamsterInstance, 'nameExists', `("${randomNames[i]}")`);
             let resultValue = (await result.decode('bool')).value;
             assert.ok(resultValue === 0, 'Hamster does not exist!');
         }
