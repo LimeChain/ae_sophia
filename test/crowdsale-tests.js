c7c7f69c745f13db97b418bb6754e1207cdcf7cd
Aleksey <alexeitcekov@abv.bg>
2019-01-11 17:06:00 +0200

    capped crowdsale contract and tests

--- test/crowdsale-tests.js
+++ test/crowdsale-tests.js
@@ -6,13 +6,12 @@ chai.use(chaiAsPromised);
 const assert = chai.assert;
 const AeSDK = require('@aeternity/aepp-sdk');
 const Universal = AeSDK.Universal;
-const chain = AeSDK.Chain; // require('@aeternity/aepp-sdk/es/chain');
 const config = require("./config.json")
 const utils = require('./utils');
-const errorMessages = require('./error-messages');
+const errorMessages = require('./error-messages').crowdsale;
 
-const sourceFile = "./../contracts/crowdsale/crowdsale.aes";
-const erc20Path = "./../contracts/erc20/erc20.aes";
+const sourceFile = "./../contracts/crowdsale/crowdsale-capped.aes";
+const erc20CappedPath = "./../contracts/erc20/erc20_capped.aes";
 
 const getDeployedContractInstance = utils.getDeployedContractInstance;
 const executeSmartContractFunction = utils.executeSmartContractFunction;
@@ -20,57 +19,38 @@ const executeSmartContractFunctionFromAnotherClient = utils.executeSmartContract
 const getAEClient = utils.getAEClient;
 const publicKeyToHex = utils.publicKeyToHex;
 const decodedHexAddressToPublicAddress = utils.decodedHexAddressToPublicAddress;
-const execute = utils.execute;
 
 let RANDOM_ADDRESS_1 = 'ak_gLYH5tAexTCvvQA6NpXksrkPJKCkLnB9MTDFTVCBuHNDJ3uZv';
 let RANDOM_ADDRESS_2 = 'ak_zPoY7cSHy2wBKFsdWJGXM7LnSjVt6cn1TWBDdRBUMC7Tur2NQ';
 let RANDOM_ADDRESS_3 = 'ak_tWZrf8ehmY7CyB1JAoBmWJEeThwWnDpU4NadUdzxVSbzDgKjP';
 
-async function getContractInstance(contractPath) {
+async function getContractInstance(contractPath, initState = undefined) {
     let notOwnerConfig = JSON.parse(JSON.stringify(config));
     notOwnerConfig.ownerKeyPair = config.notOwnerKeyPair;
 
     const sf = fs.readFileSync(path.resolve(__dirname, contractPath), 'utf8');
-    let contractInfo = await getDeployedContractInstance(Universal, notOwnerConfig, sf);
+    let contractInfo = await getDeployedContractInstance(Universal, notOwnerConfig, sf, initState);
     return contractInfo.deployedContract;
 }
 
-function extractBalance(accountInfo) {
-    if (!accountInfo) {
-        throw new Error('Account info cannot be null or empty string.')
-    }
-
-    let match = accountInfo.match(/(?:balance_+\s+(\d+))/m);
-    if (match.length >= 2) {
-        return match[1];
-    }
-
-    throw new Error('Missing balance or invalid has been changed!')
-}
-
-//let result = await executeSmartContractFunction(contractInstance, 'tknRaised');
-// let aa  = await chain.compose(EpochContract)({
-//     url: 'https://sdk-testnet.aepps.com',
-//     internalUrl: 'https://sdk-testnet.aepps.com',
-// });
-
-// let accountInfo = await execute('aecli', 'inspect', [`${config.ownerKeyPair.publicKey}`, `-u`, 'http://localhost:3001']);
-// console.log(extractBalance(accountInfo));
-
 describe('Crowdsale', () => {
 
-    describe('Simple crowdsale', async () => {
+    describe('Capped Crowdsale rate 1', async () => {
         let contractInstance;
         let anotherClientConfiguration;
         let erc20Instance;
         let DEFAULT_RATE = 1;
+        let TOKENS_TO_MINT = 10000;
 
         beforeEach(async () => {
-            erc20Instance = await getContractInstance(erc20Path);
+            erc20Instance = await getContractInstance(erc20CappedPath, `(${TOKENS_TO_MINT})`);
 
             const sf = fs.readFileSync(path.resolve(__dirname, sourceFile), 'utf8');
             let deployInfo = await getDeployedContractInstance(Universal, config, sf, `(${publicKeyToHex(erc20Instance.address)}, ${publicKeyToHex(config.ownerKeyPair.publicKey)}, ${DEFAULT_RATE})`);
             contractInstance = deployInfo.deployedContract;
+            
+            // mint tokens 
+            await executeSmartContractFunction(erc20Instance, 'mint', `(${publicKeyToHex(contractInstance.address)}, ${TOKENS_TO_MINT - 1})`);
 
             // create second/another client configuration
         	let anotherClient = await getAEClient(Universal, config, config.notOwnerKeyPair);
@@ -87,7 +67,6 @@ describe('Crowdsale', () => {
             let addressAsHex = (await result.decode('address')).value;
 
             let publicKey = decodedHexAddressToPublicAddress(addressAsHex);
-            //console.log(assert);
             assert.equal(publicKey, config.ownerKeyPair.publicKey, "Inited wallet address is not the same!");
         });
 
@@ -99,27 +78,112 @@ describe('Crowdsale', () => {
         });
 
         it("Token raised should be 0.", async () => {
-            let result = await executeSmartContractFunction(contractInstance, 'tknRaised', null);                                                        
+            let result = await executeSmartContractFunction(contractInstance, 'aeRaised', null);                                                        
             let value = (await result.decode('int')).value;
 
             assert.equal(0, value, "Token raised is different from 0!");
         });
 
-        it.only("Should buy tokens with rate 1 and token raised should be the same.", async () => {
-            // let result = await executeSmartContractFunction(contractInstance, 'get'); 
-            // let addressAsHex = (await result.decode('address')).value;
-            // let publicKey = decodedHexAddressToPublicAddress(addressAsHex);
-            // console.log(publicKey);
-            // console.log(erc20Instance);
+        it("Should buy tokens with rate 1 and token raised and balance should be equal to spent aes.", async () => {
+            let aes = 100;
+            
+            await executeSmartContractFunctionFromAnotherClient(anotherClientConfiguration, 'buyTokens', `(${publicKeyToHex(RANDOM_ADDRESS_3)})`, aes);
+
+            let result = await executeSmartContractFunction(contractInstance, 'aeRaised', null);                                                        
+            let aeRaised = (await result.decode('int')).value;
+            
+            result = await executeSmartContractFunction(erc20Instance, 'balanceOf', `(${publicKeyToHex(RANDOM_ADDRESS_3)})`);
+            let balanceOf = (await result.decode('int')).value;
+
+            assert.equal(aes, aeRaised, "Token raised is incorrect!");
+            assert.equal(aes, balanceOf, "Balance is not equal to bought tokens!");
+        });
+
+        it("Should buy tokens for 2 different accounts with rate 1. Token raised should be a sum of buy1 and buy2 tx and balance of each address should be equal to spent aes.", async () => {
+            let aesAddress1 = 100;
+            let aesAddress2 = 230;
+
+            await executeSmartContractFunction(contractInstance, 'buyTokens', `(${publicKeyToHex(RANDOM_ADDRESS_1)}, ${publicKeyToHex(erc20Instance.address)})`, aesAddress1);
+            await executeSmartContractFunctionFromAnotherClient(anotherClientConfiguration, 'buyTokens', `(${publicKeyToHex(RANDOM_ADDRESS_3)})`, aesAddress2);
+            
+            let result = await executeSmartContractFunction(contractInstance, 'aeRaised', null);                                                        
+            let aeRaised = (await result.decode('int')).value;
+
+            result = await executeSmartContractFunction(erc20Instance, 'balanceOf', `(${publicKeyToHex(RANDOM_ADDRESS_1)})`);
+            let balanceOfAddress1 = (await result.decode('int')).value;
+
+            result = await executeSmartContractFunction(erc20Instance, 'balanceOf', `(${publicKeyToHex(RANDOM_ADDRESS_3)})`);
+            let balanceOfAddress2 = (await result.decode('int')).value;
+
+            assert.equal(aesAddress1 + aesAddress2, aeRaised, "Token raised is incorrect!");
+            assert.equal(aesAddress1, balanceOfAddress1, "Balance is not equal to bought tokens!");
+            assert.equal(aesAddress2, balanceOfAddress2, "Balance is not equal to bought tokens!");
+        });
+    });
+
+    describe('Capped Crowdsale rate 5', async () => {
+        let contractInstance;
+        let anotherClientConfiguration;
+        let erc20Instance;
+        let DEFAULT_RATE = 5;
+        let TOKENS_TO_MINT = 1000;
+
+        async function mintTokens(tokenAmount) {
+            await executeSmartContractFunction(erc20Instance, 'mint', `(${publicKeyToHex(contractInstance.address)}, ${tokenAmount})`);
+        }
+
+        beforeEach(async () => {
+            erc20Instance = await getContractInstance(erc20CappedPath, `(${TOKENS_TO_MINT})`);
+
+            const sf = fs.readFileSync(path.resolve(__dirname, sourceFile), 'utf8');
+            let deployInfo = await getDeployedContractInstance(Universal, config, sf, `(${publicKeyToHex(erc20Instance.address)}, ${publicKeyToHex(config.ownerKeyPair.publicKey)}, ${DEFAULT_RATE})`);
+            contractInstance = deployInfo.deployedContract;
+
+            // create second/another client configuration
+        	let anotherClient = await getAEClient(Universal, config, config.notOwnerKeyPair);
+
+        	anotherClientConfiguration = {
+        		client: anotherClient,
+        		byteCode: deployInfo.compiledContract.bytecode,
+        		contractAddress: contractInstance.address
+        	}
+        });
+
+        it("[NEGATIVE] There is NO tokens mint, should NOT buy tokens.", async () => {
+            let aes = 100;
+            await assert.isRejected(executeSmartContractFunctionFromAnotherClient(anotherClientConfiguration, 'buyTokens', `(${publicKeyToHex(RANDOM_ADDRESS_3)})`, aes), errorMessages.NOT_ENOUGH_BALANCE);
+        });
+
+        it("[NEGATIVE] Should NOT buy more tokens than contract is minted (owner of tokens have).", async () => {
+            await mintTokens(10);
 
             let aes = 100;
-            result = await executeSmartContractFunction(contractInstance, 'buyTokens', `(${publicKeyToHex(config.ownerKeyPair.publicKey)}, ${publicKeyToHex(erc20Instance.address)})`, aes); 
             
-            console.log('==> check for token raise...');
-            result = await executeSmartContractFunction(contractInstance, 'tknRaised', null);                                                        
-            let value = (await result.decode('int')).value;
+            await assert.isRejected(executeSmartContractFunctionFromAnotherClient(anotherClientConfiguration, 'buyTokens', `(${publicKeyToHex(RANDOM_ADDRESS_3)})`, aes), errorMessages.NOT_ENOUGH_BALANCE);
+        });
 
-            assert.equal(aes, value, "Token raised is different from 0!");
+        it("[NEGATIVE] Should NOT buy more tokens than cap.", async () => {
+            await mintTokens(TOKENS_TO_MINT);
+
+            let aes = TOKENS_TO_MINT + 1;
+            
+            await assert.isRejected(executeSmartContractFunctionFromAnotherClient(anotherClientConfiguration, 'buyTokens', `(${publicKeyToHex(RANDOM_ADDRESS_3)})`, aes), errorMessages.REQUESTED_AMOUNT_BIGGER_THAN_CAP);
+        });
+
+        it("Should buy tokens with rate 5, balance should be equal to spent aes multiply by 5 and ae raised should be equal to spent aes.", async () => {
+            await mintTokens(TOKENS_TO_MINT);
+            let aes = 100;
+            
+            await executeSmartContractFunction(contractInstance, 'buyTokens', `(${publicKeyToHex(RANDOM_ADDRESS_3)})`, aes);
+
+            let result = await executeSmartContractFunction(contractInstance, 'aeRaised');                                                        
+            let aeRaised = (await result.decode('int')).value;
+            
+            result = await executeSmartContractFunction(erc20Instance, 'balanceOf', `(${publicKeyToHex(RANDOM_ADDRESS_3)})`);
+            let balanceOf = (await result.decode('int')).value;
+
+            assert.equal(aeRaised, aes, "Token raised is incorrect!");
+            assert.equal(aes * DEFAULT_RATE, balanceOf, "Balance is not equal to bought tokens!");
         });
     });
 });
